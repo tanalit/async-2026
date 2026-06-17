4fa060f0b9b2771227fd8c7445dc1cfabf739876
tanalit <nicetanalit.2548@gmail.com>
2026-06-17 10:37:49 +0700

    web

---
+++ WX/web.py
@@ -0,0 +1,17 @@
+from ast import main
+import asyncio
+#import httpx
+
+async def get_usr_name(user_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        return response.json()["name"]
+    
+    async def main():
+        name1, name2 = await asyncio.gather(get_usr_name(1), get_usr_name(2))
+        print(f"User 1: {name1} | User 2: {name2}")
+        
+if __name__ == "__main__":
+    asyncio.run(main())
